c06e5bff91b4b9693d3c5604273c5a6f80bb0f07
romana <kayembejo1@gmail.com>
2025-02-10 06:08:17 +0200

    uilogin

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'ngmradi-mlks',
+        appId: '1:1072206145282:web:f3ca0b661edf91d925841b',
+        storageBucket: 'ngmradi-mlks.firebasestorage.app',
+        apiKey: 'AIzaSyB2OVqzA31WLd5YkI3sfD-FO2_6nDiEpZo',
+        authDomain: 'ngmradi-mlks.firebaseapp.com',
+        messagingSenderId: '1072206145282',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { APP_NAME, COMPANY_NAME } from '../../app.constants';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  template: ` <p>login works!</p> `,
-  styles: ``,
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './login.compnent.html',
+  styles: `
+   .divider {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 1rem 0;
+
+      mat-divider {
+        width: 35%;
+      }
+    }
+  `,
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  appName = APP_NAME;
+  companyName = COMPANY_NAME;
+  date = new Date();
+  loginWithGoogle() {}
+}
